4716aefbdda4bd2d27ca57d476bf96344895c4d8
g-tejas <ttejasgarrepally@gmail.com>
2023-11-01 11:50:10 +0800

    added git configs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -44,12 +44,17 @@
   };
 
   # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
+  programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+
+	programs.git = {
+		enable = true;
+		userName = "g-tejas";
+		userEmail = "ttejasgarrepally@gmail.com";
+	};
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
