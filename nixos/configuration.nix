37541dd105808bdc219a8e38854e18c4966fbe23
g-tejas <ttejasgarrepally@gmail.com>
2023-12-15 09:45:57 +0800

    need this for flatpak

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -79,6 +79,8 @@
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
 
+  fonts.fontDir.enable = true;
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -132,8 +134,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     vim
      wget
+     home-manager
   ];
 
   programs.neovim = {
