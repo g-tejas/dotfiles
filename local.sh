784de5ea8d2ce41dbdf6384e5610c1ad281ae07b
Tejas Garrepally <code@gtejas.com>
2024-08-09 12:13:51 +0800

    feat: add dotfiles for nonssh bootstrapping

---
+++ local.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# If we don't use SSH, host pc requires python3, ansible and git to be installed
+# at the very minimum. This script will install the necessary dependencies.
+
+echo "Choose your operating system:"
+echo "1. Darwin"
+echo "2. Ubuntu"
+echo "3. RedHat"
+
+read -p "Enter the number corresponding to your operating system: " result
+
+setup_darwin() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install python3 ansible git
+}
+
+setup_ubuntu() {
+  sudo apt-get update
+  sudo apt-get install -y python3 ansible git
+  bash bootstrap.sh
+}
+
+setup_redhat() {
+  sudo yum update
+  sudo yum install -y python3 ansible git
+  bash bootstrap.sh
+}
+
+if [ "$result" = "1" ]; then
+  setup_darwin
+elif [ "$result" = "2" ]; then
+  setup_ubuntu
+elif [ "$result" = "3" ]; then
+  setup_redhat
+else
+  echo "Invalid input. Please choose a valid option."
+fi
